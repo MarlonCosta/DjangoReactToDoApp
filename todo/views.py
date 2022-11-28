34a1d7a56441cec5a459735487d4dd7706ceead8
MarlonCosta <mrlnrnn@gmail.com>
2022-11-27 21:02:54 -0300

    Fixing git root folder

---
+++ todo/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+
+from todo.models import Todo
+from todo.serializers import TodoSerializer
+
+
+# Create your views here.
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
